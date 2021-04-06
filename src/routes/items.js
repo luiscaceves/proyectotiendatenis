253f4a23feadc91e318affd7dcb22524c3fba72b
MarceloRodriguez <mrg0809@gmail.com>
2021-04-06 17:16:32 -0500

    editar y borrar articulos

--- src/routes/items.js
+++ src/routes/items.js
@@ -19,13 +19,41 @@ router.post('/add', async (req, res) => {
         img
     };
     await pool.query('INSERT INTO items set ?', [newItem]);
-    res.send('recieved');
+    res.redirect('/items');
 });
 
 router.get('/', async (req, res) => {
     const items = await pool.query('SELECT * FROM items');
     console.log(items);
     res.render('items/list', {items});
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM items WHERE ID = ?', [id]);
+    res.redirect('/items');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const items = await pool.query('SELECT * FROM items WHERE ID = ?', [id]);
+    res.render('items/edit', {item: items[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { model, family, brand, category, subcategory, description, img } = req.body;
+    const newItem = {
+        model,
+        family,
+        brand,
+        category,
+        subcategory,
+        description,
+        img
+    };
+    await pool.query('UPDATE items set ? WHERE id = ?', [newItem, id]);
+    res.redirect('/items');
 });
 
 module.exports = router;
